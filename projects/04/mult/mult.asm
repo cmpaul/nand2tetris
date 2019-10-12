90e5a50d95037fcf65745c84bbc157dd266dfb25
Chris <cpaul@squareup.com>
2019-10-12 16:37:33 -0700

    Week 4 - mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// for (i=0; i<R0; i++) {
+//   R2 += R1
+// }
+
+@i
+M=0 // i = 0
+@R2
+M=0 // reset R2
+
+(LOOP)
+  // if (i == R0) GOTO END
+  @i
+  D=M
+  @R0
+  D=D-M
+  @END
+  D;JEQ 
+
+  // R2 += R1
+  @R1
+  D=M
+  @R2
+  M=M+D
+
+  // i = i + 1
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
